597304e55d27f2d3ca5efd3c60af992972da7664
AlexFP <florianprescott@googlemail.com>
2018-05-01 13:17:49 -0400

    working scraper

--- controllers/newsCtrl.js
+++ controllers/newsCtrl.js
@@ -24,10 +24,10 @@ module.exports = {
 	},
 	restock: function(req, res) {
 		
-		sectionScrape = function(sphere) {
+		sectionScrape = function(region) {
 
 			axios
-				.get("https://news.un.org/en/news/region/" + sphere)
+				.get("https://news.un.org/en/news/region/" + region)
 				.then((page) => {
 
 					const $ = cheerio.load(page.data);
@@ -35,17 +35,17 @@ module.exports = {
 					$("div.views-row").each((i, element) => {
 
 						const result = {
-							sphere: sphere,
-							date: $(element).find("div.body-wrapper").children("div.story-date").text(),
-							title: $(element).find("div.body-wrapper").children("h1.story-title").text(),
-							link: $(element).find("div.body-wrapper").children("h1.story-title").has("a").attr("href"),
-							category: $(element).find("div.body-wrapper").children("div.topics").text(),
+							sphere: region,
+							date: $(element).find("div.story-date").text(),
+							title: $(element).find("h1.story-title").text(),
+							link: "https://news.un.org" + $(element).find("h1.story-title").children("a").attr("href"),
+							category: $(element).find("div.topics").text(),
 							summary: $(element).find("div.body-wrapper").children("div.news-body").text(),
 							imageLink: $(element).find("img").attr("src"),
-							imageAuthor: $(element).find("div.field-name-field-scald-photo-credit").children("span.un-news-teaser-scald-credit")
+							imageAuthor: $(element).find("div.field-name-field-scald-photo-credit").children("span.un-news-teaser").text()
 						};
 
-						if(result.title && result.link) {
+						if(result.title) {
 							db.NewsArticle
 								.create(result)
 								.then((dbNewsArt) => console.log(dbNewsArt))
@@ -54,16 +54,21 @@ module.exports = {
 
 					});
 
-					res.send("Articles scraped from https://news.un.org/en/news/region/" + sphere);
+					console.log("Articles scraped" + region);
 
 				})
-				.catch((err) => res.json(err));
+				.catch((err) => {
+					res.json(err);
+					console.log(err);
+				});
 
 		}
 
-		const spheres = ["africa", "americas", "asia-pacific", "middle-east", "europe"];
+		const regions = ["africa", "americas", "asia-pacific", "middle-east", "europe"];
 
-		sectionScrape("africa");
+		db.NewsArticle.remove({}, regions.forEach((region) => {
+			sectionScrape(region);
+		}), res.send("Articles scraped from https://news.un.org"));
 
 	}
 }
