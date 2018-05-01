d78e76bb5c8dd8bd671a478086a432f2ebc23d38
AlexFP <florianprescott@googlemail.com>
2018-04-30 22:34:10 -0400

    model

--- models/NewsArticle.js
+++ models/NewsArticle.js
@@ -1,7 +1,30 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-	// 
+	sphere: {
+		type: String,
+		require: true
+	},
+	date: {
+		type: String,
+		require: true
+	},
+	title: {
+		type: String,
+		require: true
+	},
+	link: {
+		type: String,
+		require: true
+	},
+	category: {
+		type: String,
+		require: true
+	},
+	summary: {
+		type: String,
+		require: true
+	}
 });
 
 module.exports = mongoose.model("NewsArticle", articleSchema);
