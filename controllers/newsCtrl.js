516fc46e6581bd45da5e21a1c1f4ee3a95219878
AlexFP <florianprescott@googlemail.com>
2018-04-30 22:12:36 -0400

    comment fix

--- controllers/newsCtrl.js
+++ controllers/newsCtrl.js
@@ -17,9 +17,10 @@ module.exports = {
 			.catch(err => res.status(400).json(err));
 	},
 	comment: function(req, res) {
-		.findOneAndUpdate({_id: req.params.id}, req.body)
-		.then(dbModel => res.json(dbModel))
-		.catch(err => res.status(400).json(err))
+		db.NewsArticle
+			.findOneAndUpdate({_id: req.params.id}, req.body)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(400).json(err));
 	},
 	restock: function(req, res) {
 		
