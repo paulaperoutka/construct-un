fb70ba798014b4b0411ad930f7320e4d72853d5e
AlexFP <florianprescott@googlemail.com>
2018-04-30 21:13:21 -0400

    tweaks

--- api_routes/newsAPI.js
+++ api_routes/newsAPI.js
@@ -6,12 +6,12 @@ const
 	router = express.Router();
 	newsCtrl = controllers.newsCtrl;
 
-router.route("/").get(newsCtrl.findAll);
+router.route("/").get(newsCtrl.findAll)
 
-router.router("/:id")
+router.route("/:id")
 	.get(newsCtrl.findById)
-	.put(newsCtrl.comment);
+	.put(newsCtrl.comment)
 
-router.route("/:refresh").get(newsCtrl.restock);
+router.route("/refresh").get(newsCtrl.restock)
 
-module.exports = ("newsAPI", router);
+module.exports = router;
