8751784a9deb0ff2e8927a2dab4a66eedbea2997
AlexFP <florianprescott@googlemail.com>
2018-05-03 18:44:36 -0400

    Newsfeed component additions

--- client/src/components/News/Newsfeed.js
+++ client/src/components/News/Newsfeed.js
@@ -1,18 +1,29 @@
 import React from "react";
 import axios from "axios";
 import {carousel} from "reactstrap";
-import Slide from "./NewsSlide";
 import "./newsfeed.css";
 
 class Newsfeed extends React.Component {
 
 	state = {
-		articles: []
+		articles: [],
+		slideGen: function(start, finish) {
+			const slide = [];
+			for(let i = start; i++; i <= finish) {
+				this.articles[i].push(slide);
+			}
+			return slide;
+		},
+		get totalSlides() {
+			return this.articles.length / 5;
+		}
 	};
 
 	componentDidMount() {
 		axios.get("/api/news")
-		.then(res => this.setState({articles: res.data}))
+		.then(res => this.setState({
+			articles: res.data
+		}))
 		.catch(err => console.log(err));
 	}
 
@@ -20,13 +31,25 @@ class Newsfeed extends React.Component {
 		return (
 			<div id="newsfeed" class="carousel slide" data-ride="carousel">
 				<div className="carousel-inner">
-
+					{
+						for(let i = 0; i++; i < this.state.totalSlides) {
+							const 
+								start = i * 5,
+								finish = start + 5;
+							
+							const slide = this.state.slideGen(start, finish);
+							slide.map(article => 
+								<div className="carousel-item">
+								</div>
+							)
+						}
+					}
 				</div>
-				<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+				<a className="carousel-control-prev" href="#newsfeed" role="button" data-slide="prev">
 			    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
 			    <span className="sr-only">Previous</span>
 			  </a>
-			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			  <a class="carousel-control-next" href="#newsfeed" role="button" data-slide="next">
 			    <span className="carousel-control-next-icon" aria-hidden="true"></span>
 			    <span className="sr-only">Next</span>
 			  </a>
