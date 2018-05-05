014dfe448f9d07359b3f1b2ee32a5e808e535f1b
paulaperoutka <paulaperoutka@gmail.com>
2018-05-05 05:26:22 -0400

    background rendering on portal

--- client/src/components/Portal/Portal.js
+++ client/src/components/Portal/Portal.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import "./Portal.css";
 
 class Portal extends Component {
 	state = {
@@ -8,22 +9,24 @@ class Portal extends Component {
 
 	render() {
 		return(
-			<Form>
-				<FormGroup>
-					<FormText color="muted">
-            Welcome Ambassador. Please select the country you represent.
-          </FormText>
-	          <Label for="countrySelect">Select your country</Label>
-	          <Input type="select" name="select" id="countrySelect">
-	            <option>United States of America</option>
-	            <option>Russia</option>
-	            <option>United Kingdom</option>
-	            <option>China</option>
-	            <option>France</option>
-	          </Input>
-	          <Button>Enter the UN</Button>
-	        </FormGroup>
-	      </Form>
+			<div className="wrapper">
+				<Form>
+					<FormGroup>
+						<FormText color="muted">
+	            Welcome Ambassador. Please select the country you represent.
+	          </FormText>
+		          <Label for="countrySelect">Select your country</Label>
+		          <Input type="select" name="select" id="countrySelect">
+		            <option>United States of America</option>
+		            <option>Russia</option>
+		            <option>United Kingdom</option>
+		            <option>China</option>
+		            <option>France</option>
+		          </Input>
+		          <Button>Enter the UN</Button>
+		        </FormGroup>
+		      </Form>
+		    </div>
 		);
 	}
 }
