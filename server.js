2f7343c1468e5ce139be3d3e9afb833c2e20609d
Tobia Giachetti <31292513+tgiachett@users.noreply.github.com>
2018-05-05 08:13:36 -0400

    Svg hover (#23)
    * Highlights for onMouseOver, but highlights all of them
    
    * Added onMouseOver highlight, bug: targets all fills

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === "production") {
 }
 
 app.use("/api/news", routes.newsAPI);
-app.use("/api/resolution", routes.resolutionAPI);
+// app.use("/api/resolution", routes.resolutionAPI);
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
@@ -28,3 +28,58 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ConstructUN");
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
+
+// The websocket server. WIP. Could be external file(?)
+
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8989 })
+
+const users = []
+
+const broadcast = (data, ws) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data))
+    }
+  })
+}
+
+wss.on('connection', (ws) => {
+  let index
+  ws.on('message', (message) => {
+    const data = JSON.parse(message)
+    switch (data.type) {
+      case 'ADD_USER': {
+        index = users.length
+        users.push({ name: data.name, id: index + 1 })
+        ws.send(JSON.stringify({
+          type: 'USERS_LIST',
+          users
+        }))
+        broadcast({
+          type: 'USERS_LIST',
+          users
+        }, ws)
+        break
+      }
+      case 'ADD_MESSAGE':
+        broadcast({
+          type: 'ADD_MESSAGE',
+          message: data.message,
+          author: data.author
+        }, ws)
+        break
+      default:
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    users.splice(index, 1)
+    broadcast({
+      type: 'USERS_LIST',
+      users
+    }, ws)
+  })
+})
