43021434a3731186c288baf3d74354b0a33c242b
Alexander Florian-Prescott <31100098+EPluribusAlex@users.noreply.github.com>
2018-05-05 11:22:14 -0400

    news api mostly finished, reactstrap set (#14)
    * various model, route, and controller tweaks
    
    * .gitignore test
    
    * Delete 3bb7c96e9d6d17e83bf863d3991a94c08bb7f5c3.json.gz
    
    * Delete 564cffc3deec65803a5b182a443ee07fa6ce8615.json.gz
    
    * Delete cbdee3951ef9e54679ebaf64acf3bdc638aba33f.json.gz
    
    * Delete e8e3cd2fdc97240e320b5ed8859cdaeeec9a071b.json.gz
    
    * comprehensive .gitignore
    
    * yarn fix
    
    * yarn
    
    * yarn
    
    * news components basic framework
    
    * Newsfeed component additions
    
    * yarn
    
    * fixed yarn, added bootstrap and reactstrap
    
    * newsfeed logic tweaks
    
    * added imports
    
    * yarn dependency
    
    * working on newsfeed component, problems with reactstrap
    
    * news component
    
    * removed unnecessary component file
    
    * slight style tweaks

---
+++ client/src/components/News/Newsfeed.js
@@ -0,0 +1,111 @@
+import React from "react";
+import axios from "axios";
+import {
+	Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Button,
+  CardBody,
+  Card, 
+  Collapse
+} from "reactstrap";
+import "./newsfeed.css";
+
+let items = [];
+
+class Newsfeed extends React.Component {
+
+	componentDidMount() {
+		axios.get("/api/news")
+		.then(res => res.data
+      .forEach(item => items.push(item)))
+		.catch(err => console.log(err));
+	}
+
+	constructor(props) {
+    super(props);
+    this.state = { 
+      activeIndex: 0,
+      collapse: false 
+    };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }	
+
+	render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.imageLink}
+        >
+          <img src={item.imageLink} alt="*" />
+          <CarouselCaption captionText={item.title} captionHeader={item.title} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <div>
+        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Headlines</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+              <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}
+              >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+              </Carousel>
+            </CardBody>
+          </Card>
+        </Collapse>
+      </div>
+    );
+  }
+
+}
+
+export default Newsfeed;
