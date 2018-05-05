46bb55565e3796543d601a051b912e56903a7094
paulaperoutka <paulaperoutka@gmail.com>
2018-05-05 05:13:24 -0400

    portal route working

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Portal from "./components/Portal";
+
+// import World from './components/Map';
+// import Chambers from './components/Chambers';
+// import Chat from './components/Chat';
+
+import { Container } from "reactstrap";
 import './App.css';
-import World from './components/Map';
-import Chambers from './components/Chambers';
-import Chat from './components/Chat';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <World />
-        <Chambers />
-        <Chat />
-      </div>
-      
-
+      <Router>
+        <Container>
+          <Route exact path="/" component={Portal} />
+        </Container>
+      </Router>
     );
   }
 }
 
 export default App;
+
+// <World />
+// <Chambers />
+// <Chat />
+
+
