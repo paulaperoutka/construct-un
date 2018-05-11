12e201f2344bf0f0130db3a33badbd4694b44957
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-05-11 10:24:01 -0400

    fixed modal svg viewbox to country path bbox

---
+++ client/src/components/CountryModal/CountryModal.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class CountryModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    
+    };
+     
+    this.toggle = this.toggle.bind(this);
+  }
+
+  bound = (xy) => {
+    return document.getElementById(xy).getBBox()
+    }
+
+  componentDidMount() {
+    this.box = [this.bound(this.props.id).x, this.bound(this.props.id).y, this.bound(this.props.id).width, this.bound(this.props.id).height]
+  }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  
+  render() {
+    return (
+      <foreignObject>
+        
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Country Name prop</ModalHeader>
+          <ModalBody>
+          <svg preserveAspectRatio="true" xmlns="http://www.w3.org/2000/svg" viewBox={this.box} version={1.0} height={.50*(window.innerHeight)} width={498}>
+              <path id={this.props.id} d={this.props.dForModal} style={{fill: '#b9b9b9'}}   />
+            </svg>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </foreignObject>
+    );
+  }
+}
+
+export default CountryModal;
