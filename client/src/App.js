34a56141bbb94b1624571169e049e265867784da
paulaperoutka <paulaperoutka@gmail.com>
2018-05-14 10:37:51 -0400

    add resolutions page and component

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,13 @@
 import React, { Component } from 'react';
-// import {BrowserRouter as Router, Route} from "react-router-dom";
-// import { 
-//   Container, 
-//   Row, 
-//   Col,
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   UncontrolledDropdown,
-//   DropdownToggle,
-//   DropdownMenu,
-//   DropdownItem
-// } from "reactstrap";
-// import logo from './logo.svg';
-// import './App.css';
-// import World from './components/Map';
-// import Chambers from './components/Chambers';
-// import Chat from './components/Chat';
-// import Newsfeed from "./components/News";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Portal from "./pages/Portal";
 import ChambersPage from "./pages/ChambersPage";
+import ResolutionssPage from "./pages/ResolutionsPage";
 import './App.css';
 
 class App extends Component {
-
-  // constructor(props) {
-  //   super(props);
-
-  //   this.toggle = this.toggle.bind(this);
-  //   this.state = {
-  //     isOpen: false
-  //   };
-  // }
-
-  // toggle() {
-  //   this.setState({
-  //     isOpen: !this.state.isOpen
-  //   });
-  // } 
 
   render() {
     return (
@@ -52,6 +15,7 @@ class App extends Component {
         <div>
           <Route exact path="/" component={Portal} />
           <Route exact path="/securitycouncil" component={ChambersPage} />
+          <Route exact path="/resolutions" component={ResolutionsPage} />
         </div>
       </Router>
     );
---
+++ client/src/pages/ResolutionsPage/ResolutionsPage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Col, Container } from 'reactstrap';
+import Navbar from "../../components/Navbar";
+import Newsfeed from '../../components/Newsfeed';
+import World from '../../components/Map';
+import Resolution from '../../components/Resolution';
+import Chat from '../../components/Chat';
+import "./ChambersPage.css";
+
+class ChambersPage extends Component {
+	state = {
+		country: []
+	}
+
+	render() {
+		return(
+			<div className="page-wrapper">
+				<Navbar />
+				<Container>
+					<Row>
+						<Col md="4">
+							<Newsfeed />
+						</Col>
+						<Col md="8">
+							<World />
+							<Resolution />
+							<Chat />
+						</Col>
+					</Row>
+				</Container>
+		   </div>
+		);
+	}
+}
+
+export default ChambersPage;
