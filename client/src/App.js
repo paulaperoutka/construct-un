12fd806e19d57e2ebeaf7be30b27ad5d60c17dcf
paulaperoutka <paulaperoutka@gmail.com>
2018-05-15 06:57:14 -0400

    app.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Portal from "./pages/Portal";
 import ChambersPage from "./pages/ChambersPage";
-import ResolutionssPage from "./pages/ResolutionsPage";
+import ResolutionsPage from "./pages/ResolutionsPage";
 import './App.css';
 
 class App extends Component {
