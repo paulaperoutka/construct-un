615dedaf2bd29bb4ee8cd83a85902af8607bc245
paulaperoutka <paulaperoutka@gmail.com>
2018-05-17 13:40:11 -0400

    instructions page added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = props => (
 		<div className="navbar fixed-top">
 			<span className="navbar-brand mb-0 h1">
 				<img id="un-two" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Emblem_of_the_United_Nations.svg/2000px-Emblem_of_the_United_Nations.svg.png" alt="un-emblem"></img>
+				
 				<RenderResolution />
 			</span>
 		</div>
---
+++ client/src/components/NavbarBlank/NavbarBlank.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavbarBlank.css";
+
+const NavbarBlank = props => (
+	<div className="navbar-wrapper">
+		<div className="navbar fixed-top">
+			<span className="navbar-brand mb-0 h1">
+				<img id="un-two" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Emblem_of_the_United_Nations.svg/2000px-Emblem_of_the_United_Nations.svg.png" alt="un-emblem"></img>
+				<div id="reroute">
+					<Link to="/" outline color="secondary">Main </Link>
+					<Link to="/securitycouncil" outline color="secondary"> Security Council </Link>
+					<Link to="/unresolutions" outline color="secondary"> Resolutions</Link>
+				</div>
+			</span>
+		</div>
+	</div>
+)
+
+export default NavbarBlank;
